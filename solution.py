f25fef4fc2394f8b8bdb744c0c676d5bb20406fc
securitymike <63473027+securitymike@users.noreply.github.com>
2020-10-06 18:03:53 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect(127.0.0.1, 1025)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -25,34 +25,46 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv1 = clientSocket.recv(1024).decode()
     print(recv1)
     if recv1[:3] != '250':
-        print('250 reply not received from server.')
+        print('250 HELO reply not received from server.')
 
     # Send MAIL FROM command and print server response.
     # Fill in start
-    mailFromCommand = ('MAIL From: mjp269@nyu.edu')
+    mailFromCommand = ('MAIL From: Alice@nyu.edu')
     clientSocket.send(mailFromCommand.encode())
+    if recv1[:3] != '250':
+        print('250 MAIL FROM reply not received from server.')
     # Fill in end
 
     # Send RCPT TO command and print server response.
     # Fill in start
+    rcptToCommand = ('RCPT To: mjp269@nyu.edu')
+    clientSocket.send(rcptToCommand.encode())
+    if recv1[:3] != '250':
+        print('250 RCPT TO reply not received from server.')
     # Fill in end
 
     # Send DATA command and print server response.
     # Fill in start
+    dataCommand = ('DATA\r\n')
+    clientSocket.send(dataCommand.encode())
+    if recv1[:3] != '250':
+        print('250 DATA reply not received from server.')
     # Fill in end
 
     # Send message data.
     # Fill in start
+    clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period.
     # Fill in start
+    clientSocket.send(endmsg.encode())
     # Fill in end
 
     # Send QUIT command and get server response.
     # Fill in start
+    clientSocket.send('QUIT'.encode())
     # Fill in end
-
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
