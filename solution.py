898d2118915531cd18a044468af88db8aea84520
securitymike <63473027+securitymike@users.noreply.github.com>
2020-10-09 15:27:36 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -23,7 +23,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
+    #print(recv1)
     #if recv1[:3] != '250':
     #    print('250 HELO reply not received from server.')
 
