a28c66984e56fc2bed5f768f7d31ee07ccf18cc1
securitymike <63473027+securitymike@users.noreply.github.com>
2020-10-10 13:00:54 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -64,7 +64,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Message ends with a single period.
     # Fill in start
-    clientSocket.send(endmsg.encode())
+    #clientSocket.send(endmsg.encode())
     #recvEndMsg = clientSocket.recv(1024).decode()
     #if recvEndMsg[:3] != '250':
     #    print('250 ENDMSG reply not received from server.')
